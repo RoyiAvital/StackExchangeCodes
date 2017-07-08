3b4930cbf96117aaf1a319dc8c1c1175d3b2aa47
Royi <RoyiAvital@yahoo.com>
2017-07-08 21:52:52 +0300

    Added solution to https://stackoverflow.com/questions/44984132 - How to calculate weight to minimize variance?

--- StackOverflow/Q44984132/CalcFunGrad.m
+++ StackOverflow/Q44984132/CalcFunGrad.m
@@ -1,44 +1,41 @@
 function [ vG ] = CalcFunGrad( vX, hObjFun, difMode, epsVal )
 % ----------------------------------------------------------------------------------------------- %
-% [ vX ] = ProjectSimplex( vY, ballRadius, stopThr )
-%   Solving the Orthoginal Porjection Problem of the input vector onto the
-%   Simplex Ball using Dual Function and Newton Iteration.
+% [ vG ] = CalcFunGrad( vX, hObjFun, difMode, epsVal )
+%   Calculating the Gradient Vector of a function using Finite Differences
+%   method.
 % Input:
-%   - vY            -   Input Vector.
+%   - vX            -   Input Vector.
+%                       The point the gradient is caclaulated at.
 %                       Structure: Vector (Column).
 %                       Type: 'Single' / 'Double'.
 %                       Range: (-inf, inf).
-%   - ballRadius    -   Ball Radius.
-%                       Sets the Radius of the Simplex Ball. For Unit
-%                       Simplex set to 1.
+%   - hObjFun       -   Objective Function.
+%                       Function handler which evaluates the Objective
+%                       Function at a given point - hObjFun(vX).
+%                       Structure: Function Handler.
+%                       Type: Function Handler.
+%                       Range: NA.
+%   - difMode       -   Difference Mode.
+%                       Sets the mode of operation of the Finite
+%                       Differences Method - Forward, Backward or Central.
 %                       Structure: Scalar.
 %                       Type: 'Single' / 'Double'.
-%                       Range: (0, inf).
-%   - stopThr       -   Stopping Threshold.
-%                       Sets the trheolds of the Newton Iteration. The
-%                       absolute value of the Objective Function will be
-%                       below the threshold.
-%                       Structure: Scalar.
-%                       Type: 'Single' / 'Double'.
-%                       Range: (0, inf).
+%                       Range: {1, 2, 3}.
 % Output:
-%   - vX            -   Output Vector.
-%                       The projection of the Input Vector onto the Simplex
-%                       Ball.
+%   - vG            -   Gradient Vector.
+%                       The numerical approximation of the gradient of the
+%                       Objective Function at the input point 'vX'.
 %                       Structure: Vector (Column).
 %                       Type: 'Single' / 'Double'.
 %                       Range: (-inf, inf).
 % References
-%   1.  https://math.stackexchange.com/questions/2327504.
-%   2.  https://en.wikipedia.org/wiki/Newton%27s_method.
+%   1.  https://en.wikipedia.org/wiki/Finite_difference_coefficient.
 % Remarks:
 %   1.  a
 % TODO:
 %   1.  U.
 % Release Notes:
-%   -   1.0.001     09/05/2017  Royi Avital
-%       *   Renaming 'paramLambda' -> 'paramMu' to match derivation.
-%   -   1.0.000     09/05/2017  Royi Avital
+%   -   1.0.000     08/07/2017  Royi Avital
 %       *   First release version.
 % ----------------------------------------------------------------------------------------------- %
 
@@ -58,10 +55,19 @@ objFunRef   = hObjFun(vX);
 vG          = zeros([numElements, 1]);
 vPertVal    = zeros([numElements, 1]);
 
+switch(difMode)
+    case(DIFF_MODE_FORWARD)
+        hCalcGradFun = @(vPertVal) (hObjFun(vX + vPertVal) - objFunRef) / epsVal;
+    case(DIFF_MODE_BACKWARD)
+        hCalcGradFun = @(vPertVal) (objFunRef - hObjFun(vX - vPertVal)) / epsVal;
+    case(DIFF_MODE_CENTRAL)
+        hCalcGradFun = @(vPertVal) (hObjFun(vX + vPertVal) - hObjFun(vX - vPertVal)) / (2 * epsVal);
+end
+
 for ii = 1:numElements
-    vPertVal(ii) = epsVal;
-    vG(ii) = (hObjFun(vX + vPertVal) - objFunRef) / epsVal;
-    vPertVal(ii) = 0;
+    vPertVal(ii)    = epsVal;
+    vG(ii)          = hCalcGradFun(vPertVal);
+    vPertVal(ii)    = 0;
 end
 
 
