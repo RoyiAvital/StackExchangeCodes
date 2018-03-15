d650e93917305485257ec85bbb4ebf39f71664d8
Royi <RoyiAvital@yahoo.com>
2018-03-15 23:04:04 +0200

    Added solution to https://math.stackexchange.com/questions/2595199 - Proximal Mapping of Least Squares with L1 and L2 Mixed Norm Regularization (Similar to Elastic Net).

--- Mathematics/Q2595199/Q2595199.m
+++ Mathematics/Q2595199/Q2595199.m
@@ -27,10 +27,10 @@ generateFigures = ON;
 paramLam1 = 1;
 paramLam2 = 2;
 
-numElements = 4;
+numElements = 8;
 
-numIterations   = 6000;
-stepSize        = 0.00095;
+numIterations   = 1000;
+stepSize        = 0.0075;
 
 
 %% Generate Data
@@ -38,7 +38,10 @@ stepSize        = 0.00095;
 vB = 10 * randn([numElements, 1]);
 
 hObjFun = @(vX) (0.5 * sum((vX - vB) .^ 2)) + (paramLam1 * norm(vX, 1)) + (paramLam2 * norm(vX, 2));
-hL2SubGrad = @(vX) (norm(vX, 2) == 0) * (vX ./ (norm(vX, 2) + ((norm(vX, 2) == 0) * 1e-7)));
+hL2SubGrad = @(vX) vX ./ max(norm(vX, 2), 1e-9);
+
+hSoftThresholdL1 = @(vX, paramLambda) sign(vX) .* max(abs(vX) - paramLambda, 0);
+hSoftThresholdL2 = @(vX, paramLambda) vX .* (1 - (paramLambda / (max(norm(vX, 2), paramLambda))));
 
 
 %% Solution by CVX
@@ -57,6 +60,18 @@ disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
 disp([' ']);
 
 
+%% Analytic Solution
+
+vX = hSoftThresholdL2(hSoftThresholdL1(vB, paramLam1), paramLam2);
+analyticObjVal = hObjFun(vX);
+
+disp([' ']);
+disp(['Analytic Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(analyticObjVal)]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
+
 %% Solution by Sub Gradient Method
 
 vObjValSgm = zeros([numIterations, 1]);
@@ -68,28 +83,124 @@ for ii = 2:numIterations
     vG = (vX - vB) + (paramLam1 * sign(vX)) + (paramLam2 * hL2SubGrad(vX));
     vX = vX - (stepSize * vG);
     
-    vObjValSgm(ii) = hObjFun(vX);
-    
+    vObjValSgm(ii) = hObjFun(vX); 
 end
 
+disp([' ']);
+disp(['Sub Gradient Method Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(vObjValSgm(numIterations))]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
 
-%% Solution by ADMM
+
+%% Solution by https://doi.org/10.1007/s10957-012-0245-9 (A Primal Dual Splitting Method for Convex Optimization Involving Lipschitzian, Proximable and Linear Composite Terms)
+% Implementing Algorithm 3.2
+% The Matrix L is identity
+% The Gradient of F is vX - vB
+
+vObjValSplit = zeros([numIterations, 1]);
+
+paramSigma  = 0.05;
+paramTau    = 0.05;
+paramPhi    = 0.5;
+
+hSoftThresholdL1 = @(vX, paramLambda) sign(vX) .* max(abs(vX) - paramLambda, 0);
+hSoftThresholdL2 = @(vX, paramLambda) vX .* (1 - (paramLambda / (max(norm(vX, 2), paramLambda))));
+
+hProxG = @(vX, paramTau) hSoftThresholdL1(vX, paramTau * paramLam1); %<! Soft Thresholding L1
+hProxH = @(vX, paramSigma) hSoftThresholdL2(vX, paramSigma * paramLam2); %<! Soft Thresholding L2
+
+hProxHConj = @(vX, paramSigma) vX - (paramSigma * hProxH(vX / paramSigma, (1 / paramSigma))); %<! Moreau’s Identity
+
+vX = zeros([numElements, 1]);
+vY = zeros([numElements, 1]);
+vXX = vX;
+vYY = vY;
+
+vObjValSplit(1) = hObjFun(vX);
+
+for ii = 2:numIterations
+    vYY = hProxHConj(vY + (paramSigma * vX), paramSigma * paramLam2);
+    vXX = hProxG(vX - (paramTau * (vX - vB + (2 * vYY) - vY)), paramTau);
+    
+    vX = (paramPhi * vXX) + (1 - paramPhi) * vX;
+    vY = (paramPhi * vYY) + (1 - paramPhi) * vY;
+    
+    vObjValSplit(ii) = hObjFun(vX);
+end
+
+disp([' ']);
+disp(['Split Method Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(vObjValSplit(numIterations))]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
+
+%% Solution by ADMM (3 Blocks)
+% Using Scaled Form (See Distributed Optimization and Statistical Learning
+% via the Alternating Direction Method of Multipliers Pg. 15).
+
+vObjValAdmm = zeros([numIterations, 1]);
+
+paramRho = 0.25;
+
+mA = [eye(numElements); eye(numElements)];
+mB = [eye(numElements); zeros(numElements)];
+mC = [zeros(numElements); eye(numElements)];
+
+mI = eye(numElements);
+mII = (1 / paramRho) * mI;
+vBB = (1 / paramRho) * vB;
+
+mAA = mA.' * mA;
+mAB = mA.' * mB;
+mAC = mA.' * mC;
+
+mAAInv = inv(mAA + mII);
+
+vX = zeros([numElements, 1]);
+vY = zeros([numElements, 1]);
+vZ = zeros([numElements, 1]);
+vU = zeros([2 * numElements, 1]);
+
+vObjValAdmm(1) = hObjFun(vX);
+
+for ii = 2:numIterations
+    
+    vX = mAAInv * (vBB + (mAB * vY) + (mAC * vZ) - (mA.' * vU));
+    % vX = (mAA + mII) \ (vBB + (mAB * vY) + (mAC * vZ) - (mA.' * vU));
+    vY = SolveLsL1Prox(mB, ((mA * vX) + vU - (mC * vZ)), (paramLam1 / paramRho), numIterations);
+    vZ = SolveLsL2Prox(mC, ((mA * vX) + vU - (mB * vY)), (paramLam2 / paramRho), numIterations);
+    
+    vU = vU + (mA * vX) - (mB * vY) - (mC * vZ);
+    
+    vObjValAdmm(ii) = hObjFun(vX);
+end
+
+disp([' ']);
+disp(['ADMM 3 Blocks Method Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(vObjValAdmm(numIterations))]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
 
 
 %% Display Reesults
 
+figureIdx = figureIdx + 1;
+
 hFigure     = figure('Position', figPosLarge);
 hAxes       = axes();
-hLineSeries = plot([1:numIterations], [cvx_optval * ones([numIterations, 1]), vObjValSgm]);
+hLineSeries = plot([1:numIterations], [cvx_optval * ones([numIterations, 1]), analyticObjVal * ones([numIterations, 1]), vObjValSgm, vObjValSplit, vObjValAdmm]);
 set(hLineSeries, 'LineWidth', lineWidthNormal);
-set(hLineSeries(2), 'LineStyle', ':');
+set(hLineSeries(2:end), 'LineStyle', ':');
 set(get(hAxes, 'Title'), 'String', ['Least Squares with Mixed Norm Regularization'], ...
     'FontSize', fontSizeTitle);
 set(get(hAxes, 'XLabel'), 'String', 'Iteration Index', ...
     'FontSize', fontSizeAxis);
 set(get(hAxes, 'YLabel'), 'String', 'Objective Function Value', ...
     'FontSize', fontSizeAxis);
-hLegend = ClickableLegend({['CVX'], ['Sub Gradient Method']});
+hLegend = ClickableLegend({['CVX'], ['Analytic Solution'], ['Sub Gradient Method'], ['Split Method'], ['ADMM - 3 Blocks']});
 set(hAxes, 'LooseInset', [0.07, 0.07, 0.07, 0.07]);
 
 if(generateFigures == ON)
---
+++ Mathematics/Q2595199/SolveLsL1Prox.m
@@ -0,0 +1,74 @@
+function [ vX, mX ] = SolveLsL1Prox( mA, vB, paramLambda, numIterations )
+% ----------------------------------------------------------------------------------------------- %
+%[ vX, mX ] = SolveLsL1Prox( mA, vB, lambdaFctr, numIterations )
+% Solve L1 Regularized Least Squares Using Proximal Gradient (PGM) Method.
+% Input:
+%   - mA                -   Input Matirx.
+%                           The model matrix.
+%                           Structure: Matrix (m X n).
+%                           Type: 'Single' / 'Double'.
+%                           Range: (-inf, inf).
+%   - vB                -   input Vector.
+%                           The model known data.
+%                           Structure: Vector (m X 1).
+%                           Type: 'Single' / 'Double'.
+%                           Range: (-inf, inf).
+%   - paramLambda       -   Parameter Lambda.
+%                           The L1 Regularization parameter.
+%                           Structure: Scalar.
+%                           Type: 'Single' / 'Double'.
+%                           Range: (0, inf).
+%   - numIterations     -   Number of Iterations.
+%                           Number of iterations of the algorithm.
+%                           Structure: Scalar.
+%                           Type: 'Single' / 'Double'.
+%                           Range {1, 2, ...}.
+% Output:
+%   - vX                -   Output Vector.
+%                           Structure: Vector (n X 1).
+%                           Type: 'Single' / 'Double'.
+%                           Range: (-inf, inf).
+% References
+%   1.  Wikipedia PGM - https://en.wikipedia.org/wiki/Proximal_gradient_method.
+% Remarks:
+%   1.  Using vanilla PGM.
+% Known Issues:
+%   1.  A
+% TODO:
+%   1.  B
+% Release Notes:
+%   -   1.0.000     23/08/2017
+%       *   First realease version.
+% ----------------------------------------------------------------------------------------------- %
+
+mAA = mA.' * mA;
+vAb = mA.' * vB;
+vX  = pinv(mA) * vB; %<! Dealing with "Fat Matrix"
+
+stepSize = 1 / (2 * (norm(mA, 2) ^ 2));
+% stepSize = 1 / sum(mA(:) .^ 2); %<! Faster to calculate, conservative (Hence slower)
+
+mX = zeros([size(vX, 1), numIterations]);
+mX(:, 1) = vX;
+
+for ii = 2:numIterations
+    
+    vG = (mAA * vX) - vAb;
+    vX = ProxL1(vX - (stepSize * vG), stepSize * paramLambda);
+    
+    mX(:, ii) = vX;
+    
+end
+
+
+end
+
+
+function [ vX ] = ProxL1( vX, lambdaFactor )
+
+% Soft Thresholding
+vX = max(vX - lambdaFactor, 0) + min(vX + lambdaFactor, 0);
+
+
+end
+
