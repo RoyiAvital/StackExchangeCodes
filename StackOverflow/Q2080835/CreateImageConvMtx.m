d4832be70a17df52892ad070c3a97148f0d605d3
Royi <RoyiAvital@yahoo.com>
2019-01-16 11:35:45 +0200

    Solution to https://stackoverflow.com/questions/2080835 - Calculating Inverse Filter of Image Convolution Kernel.

--- StackOverflow/Q2080835/CreateImageConvMtx.m
+++ StackOverflow/Q2080835/CreateImageConvMtx.m
@@ -1,87 +1,82 @@
-function [ mK ] = CreateImageConvMtx( mH, numRows, numCols, operationMode, convShape )
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
+function [ mK ] = CreateImageConvMtx( mH, numRows, numCols, convShape )
+% ----------------------------------------------------------------------------------------------- %
+%Generates a Convolution Matrix for the 2D Kernel (The Matrix mH) with
+%support for different convolution shapes (Full / Same / Valid).
+% Input:
+%   - mH                -   Input 2D Convolution Kernel.
+%                           Structure: Matrix.
+%                           Type: 'Single' / 'Double'.
+%                           Range: (-inf, inf).
+%   - numRows           -   Number of Rows.
+%                           Number of rows in the output convolution
+%                           matrix.
+%                           Structure: Scalar.
+%                           Type: 'Single' / 'Double'.
+%                           Range: {1, 2, 3, ...}.
+%   - numCols           -   Number of Columns.
+%                           Number of columns in the output convolution
+%                           matrix.
+%                           Structure: Scalar.
+%                           Type: 'Single' / 'Double'.
+%                           Range: {1, 2, 3, ...}.
+%   - convShape         -   Convolution Shape.
+%                           The shape of the convolution which the output
+%                           convolution matrix should represent. The
+%                           options should match MATLAB's conv2() function
+%                           - Full / Same / Valid.
+%                           Structure: Scalar.
+%                           Type: 'Single' / 'Double'.
+%                           Range: {1, 2, 3}.
+% Output:
+%   - mK                -   Convolution Matrix.
+%                           The output convolution matrix. Multiplying in
+%                           the column stack form on an image should be
+%                           equivalent to applying convolution on the
+%                           image.
+%                           Structure: Matrix (Sparse).
+%                           Type: 'Single' / 'Double'.
+%                           Range: (-inf, inf).
+% References:
+%   1.  MATLAB's 'convmtx2()' - https://www.mathworks.com/help/images/ref/convmtx2.html.
+% Remarks:
+%   1.  gf
+% TODO:
+%   1.  
+%   Release Notes:
+%   -   1.0.000     dd/mm/yyyy  firstName lastName
+%       *   First release version.
+% ----------------------------------------------------------------------------------------------- %
 
-% Not Working! Work in Progress.
-
-OPERATION_MODE_CONVOLUTION = 1;
-OPERATION_MODE_CORRELATION = 2;
-
-CONVOLUTION_SHAPE_FULL         = 1;
-CONVOLUTION_SHAPE_SAME         = 2;
-CONVOLUTION_SHAPE_VALID        = 3;
-
-switch(operationMode)
-    case(OPERATION_MODE_CONVOLUTION)
-        mH = mH(end:-1:1, end:-1:1);
-    case(OPERATION_MODE_CORRELATION)
-        % mH = mH; %<! Default Code is correlation
-end
-
-
-numElementsImage    = numRows * numCols;
-numRowsKernel       = size(mH, 1);
-numColsKernel       = size(mH, 2);
-numElementsKernel   = numRowsKernel * numColsKernel;
-
-
-vRows = zeros(numElementsImage * numElementsKernel, 1);
-vCols = zeros(numElementsImage * numElementsKernel, 1);
-vVals = zeros(numElementsImage * numElementsKernel, 1);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-kernelLength    = length(vK);
-mK = zeros([numElements + kernelLength - 1, numElements]);
-
-for ii = 1:numElements + kernelLength - 1
-    kernelLastIdx     = min(kernelLength, kernelLength + numElements - ii);
-    kernelFirstIdx    = max(kernelLastIdx - ii + 1, 1);
-    
-    kernelEffLength = kernelLastIdx - kernelFirstIdx + 1;
-    
-    colLastIdx   = min(ii, numElements);
-    colFirstIdx  = colLastIdx - kernelEffLength + 1;
-    
-    mK(ii, colFirstIdx:colLastIdx)   = vK(kernelFirstIdx:kernelLastIdx);
-end
+CONVOLUTION_SHAPE_FULL  = 1;
+CONVOLUTION_SHAPE_SAME  = 2;
+CONVOLUTION_SHAPE_VALID = 3;
 
 switch(convShape)
     case(CONVOLUTION_SHAPE_FULL)
-        % mK = mK;
+        % Code for the 'full' case
+        convShapeString = 'full';
     case(CONVOLUTION_SHAPE_SAME)
-        rowIdxFirst = 1 + floor(kernelLength / 2);
-        rowIdxLast  = rowIdxFirst + numElements - 1;
-        mK = mK(rowIdxFirst:rowIdxLast, :);
+        % Code for the 'same' case
+        convShapeString = 'same';
     case(CONVOLUTION_SHAPE_VALID)
-        mK = mK(kernelLength:end - kernelLength + 1, :);
+        % Code for the 'valid' case
+        convShapeString = 'valid';
 end
+
+mImpulse = zeros(numRows, numCols);
+
+for ii = numel(mImpulse):-1:1
+    
+    mImpulse(ii) = 1;  %Create impulse image corresponding to i-th output matrix column
+    
+    mTmp = sparse(conv2(mImpulse, mH, convShapeString));  %impulse response
+    
+    cColumn{ii} = mTmp(:);
+    
+    mImpulse(ii) = 0;
+end
+
+mK = cell2mat(cColumn);
 
 
 end
---
+++ StackOverflow/Q2080835/TestConvMtx.m
@@ -0,0 +1,66 @@
+
+CONVOLUTION_SHAPE_FULL  = 1;
+CONVOLUTION_SHAPE_SAME  = 2;
+CONVOLUTION_SHAPE_VALID = 3;
+
+maxThr = 1e-9;
+
+
+for numRowsImage = 28:32
+    for numColsImage = 28:32
+        
+        mI = rand(numRowsImage, numColsImage);
+        
+        for numRowsKernel = 3:7
+            for numColsKernel = 3:7
+                
+                mH = rand(numRowsKernel, numColsKernel);
+                
+                for convShape = 1:3
+                    
+                    switch(convShape)
+                        case(CONVOLUTION_SHAPE_FULL)
+                            numRowsOut = numRowsImage + numRowsKernel - 1;
+                            numColsOut = numColsImage + numColsKernel - 1;
+                            
+                            convShapeString = 'full';
+                        case(CONVOLUTION_SHAPE_SAME)
+                            numRowsOut = numRowsImage;
+                            numColsOut = numColsImage;
+                            
+                            convShapeString = 'same';
+                        case(CONVOLUTION_SHAPE_VALID)
+                            numRowsOut = numRowsImage - numRowsKernel + 1;
+                            numColsOut = numColsImage - numColsKernel + 1;
+                            
+                            convShapeString = 'valid';
+                    end
+                    
+                    mORef   = conv2(mI, mH, convShapeString);
+                    mK      = CreateImageConvMtx(mH, numRowsImage, numColsImage, convShape);
+                    mO      = reshape(mK * mI(:), numRowsOut, numColsOut);
+                    
+                    disp([' ']);
+                    disp(['Validating solution for the following parameters:']);
+                    disp(['Image Size - [', num2str(numRowsImage), ' x ', num2str(numColsImage), ']']);
+                    disp(['Kernel Size - [', num2str(numRowsKernel), ' x ', num2str(numColsKernel), ']']);
+                    disp(['Convolution Shape - ', convShapeString]);
+                    
+                    mE = mO - mORef;
+                    maxAbsDev = max(abs(mE(:)));
+                    if(maxAbsDev >= maxThr)
+                        disp([' ']);
+                        disp(['Validation Failed']);
+                        disp([' ']);
+                    end
+                    assert(maxAbsDev < maxThr);
+                    
+                end
+            end
+        end
+    end
+end
+            
+
+
+
