35831c3eee8a83faa9b4400ff2020465adf30fa8
Royi <RoyiAvital@yahoo.com>
2019-11-27 13:39:59 +0200

    Solution to StackExchange Signal Processing Q62024 (https://dsp.stackexchange.com/questions/62024) - Proximal Gradient Method (PGM) for a Function Model with More than 2 Functions (Sum of Functions).

---
+++ SignalProcessing/Q61273/MLvSOthers.m
@@ -0,0 +1,84 @@
+% ML vs. Others for Multiple Unknowns Means
+% References:
+%   1.  A
+% Remarks:
+%   1.  B
+% TODO:
+% 	1.  C
+% Release Notes
+% - 1.0.000     15/10/2019
+%   *   First release.
+
+
+%% General Parameters
+
+subStreamNumberDefault = 12;
+
+run('InitScript.m');
+
+figureIdx           = 0;
+figureCounterSpec   = '%04d';
+
+generateFigures = ON;
+
+
+%% Simulation Parameters
+
+inputImageFileName  = 'InputImage.png';
+
+vNumRealizations    = 2:60;
+noiseStd            = 0.1;
+
+
+%% Generate Data
+
+mI = im2double(imread(inputImageFileName));
+
+numRows = size(mI, 1);
+numCols = size(mI, 2);
+
+tI              = mI + (noiseStd * randn(numRows, numCols, vNumRealizations(end)));
+mMeanImage      = zeros(numRows, numCols);
+tN              = zeros(numRows, numCols, vNumRealizations(end));
+mEstNoiseStd    = zeros(length(vNumRealizations), 2);
+vN              = zeros(numRows * numCols * vNumRealizations(end), 1);
+
+for ii = 1:length(vNumRealizations)
+    numRealizations             = vNumRealizations(ii);
+    mMeanImage(:)               = mean(tI(:, :, 1:numRealizations), 3);
+    tN(:, :, 1:numRealizations) = tI(:, :, 1:numRealizations) - mMeanImage;
+    numNoiseSamples             = numRows * numCols * numRealizations;
+    vN(1:numNoiseSamples)       = reshape(tN(:, :, 1:numRealizations), numNoiseSamples, 1); %<! In MATLAB R2018b and above can use 'all' to skip this
+    mEstNoiseStd(ii, 1)         = sqrt(mean(vN(1:numNoiseSamples) .^ 2)); % std(vN);
+    mEstNoiseStd(ii, 2)         = mean(reshape(std(tI(:, :, 1:numRealizations), 0, 3), numRows * numCols, 1));
+    mEstNoiseStd(ii, 3)         = sqrt(mean(reshape(var(tI(:, :, 1:numRealizations), 0, 3), numRows * numCols, 1)));
+end
+
+
+%% Analysis
+
+figureIdx = figureIdx + 1;
+
+hFigure     = figure('Position', figPosLarge);
+hAxes       = axes();
+hLineObj    = line(vNumRealizations, [mEstNoiseStd, noiseStd * ones(length(vNumRealizations), 1)]);
+set(hLineObj, 'LineWidth', lineWidthNormal);
+set(hLineObj(end), 'LineStyle', ':');
+set(get(hAxes, 'Title'), 'String', {['Estimation of the Noise STD as a Function of Number of Realizations']}, ...
+    'FontSize', fontSizeTitle);
+set(get(hAxes, 'XLabel'), 'String', {['Number of Realizations']}, ...
+    'FontSize', fontSizeAxis);
+set(get(hAxes, 'YLabel'), 'String', {['STD']}, ...
+    'FontSize', fontSizeAxis);
+hLegend = ClickableLegend({['Estimated STD - Method 1'], ['Estimated STD - Method 2'], ['Estimated STD - Method 3'], ['Ground Truth']});
+
+if(generateFigures == ON)
+    saveas(hFigure,['Figure', num2str(figureIdx, figureCounterSpec), '.png']);
+end
+
+
+%% Restore Defaults
+
+% set(0, 'DefaultFigureWindowStyle', 'normal');
+% set(0, 'DefaultAxesLooseInset', defaultLoosInset);
+
--- SignalProcessing/Q62024/Q62024.m
+++ SignalProcessing/Q62024/Q62024.m
@@ -27,8 +27,15 @@ generateFigures = OFF;
 %% Simulation Parameters
 
 numElements = 40;
-paramLambda1 = 0.1; %<! L1 Norm
-paramLambda2 = 0.05; %<! TV Norm
+paramLambda1 = 0.5; %<! L1 Norm
+paramLambda2 = 0.75; %<! TV Norm
+
+numIterations   = 50000;
+stepSize        = 0.00015;
+
+cMethodNames = {['CVX'], ['Sub Gradient Method']};
+% cMethodNames = {['CVX'], ['Sub Gradient Method'], ['Proximal Gradient Method'], ['ADMM']};
+methodIdx = 0;
 
 
 %% Generate Data
@@ -41,13 +48,16 @@ mD = spdiags([-ones(numElements, 1), ones(numElements, 1)], [0, 1], numElements 
 % Objective Function
 hObjFun = @(vX) (0.5 * sum( (vX - vY) .^ 2)) + (paramLambda1 * sum(abs(vX))) + (paramLambda2 * sum(abs(mD * vX)));
 
+numMethods  = size(cMethodNames, 1);
+mObjVal     = zeros(numIterations, numMethods);
+
 
 %% Solution by CVX
 
 cvx_begin('quiet')
     cvx_precision('best');
     variable vX(numElements);
-    minimize( 0.5 * pow_pos(norm(vX - vY, 2), 2) + (paramLambda1 * norm(vX, 1)) + (paramLambda1 * norm(mD * vX, 1)));
+    minimize( (0.5 * pow_pos(norm(vX - vY, 2), 2)) + (paramLambda1 * norm(vX, 1)) + (paramLambda2 * norm(mD * vX, 1)));
 cvx_end
 
 disp([' ']);
@@ -57,13 +67,28 @@ disp(['The Optimal Value Is Given By - ', num2str(cvx_optval)]);
 disp(['The Optimal Argument Is Given By - [ ', num2str(vX(:).'), ' ]']);
 disp([' ']);
 
+methodIdx = methodIdx + 1;
+mObjVal(:, methodIdx) =  cvx_optval * ones([numIterations, 1]);
 
-%% Solution by Prox Function (Closed Form Solution)
+
+
+%% Solution by Sub Gradient Descent
 %{
-Solving
+Solving $ \arg \min_x \frac{1}{2} {\left\| x - y \right\|}_{2}^{2} +
+{\lambda}_{1} {\left\| x \right\|}_{1} + {\lambda}_{2} {\left\| D x \right\|}_{1} $
 %}
 
-vX = ProjectWeightedL2Ball(vY, mW, vC, paramLambda);
+methodIdx = methodIdx + 1;
+
+vX = vY;
+vG = vX;
+mObjVal(1, methodIdx) = hObjFun(vX);
+
+for ii = 2:numIterations
+    vG(:) = (vX - vY) + (paramLambda1 * sign(vX)) + (paramLambda2 * mD.' * sign(mD * vX));
+    vX(:) = vX - (stepSize * vG);
+    mObjVal(ii, methodIdx) = hObjFun(vX);
+end
 
 disp([' ']);
 disp(['Projected Gradient Descent Solution Summary']);
@@ -72,12 +97,41 @@ disp(['The Optimal Argument Is Given By - [ ', num2str(vX(:).'), ' ]']);
 disp([' ']);
 
 
+%% Solution by Prox Function (Closed Form Solution)
+%{
+Solving
+%}
+
+% vX = ProjectWeightedL2Ball(vY, mW, vC, paramLambda);
+% 
+% disp([' ']);
+% disp(['Projected Gradient Descent Solution Summary']);
+% disp(['The Optimal Value Is Given By - ', num2str(hObjFun(vX))]);
+% disp(['The Optimal Argument Is Given By - [ ', num2str(vX(:).'), ' ]']);
+% disp([' ']);
+
+
 %% Display Results
 
 figureIdx = figureIdx + 1;
 
+hFigure     = figure('Position', figPosLarge);
+hAxes       = axes();
+hLineSeries = plot([1:numIterations], mObjVal);
+set(hLineSeries, 'LineWidth', lineWidthNormal);
+set(hLineSeries(2:end), 'LineStyle', ':');
+set(get(hAxes, 'Title'), 'String', {['Least Squares with {L}_{1} (LASSO) and Total Varitaion Norm Regularization'], ['SubStream - ', num2str(subStreamNumber)]}, ...
+    'FontSize', fontSizeTitle);
+set(get(hAxes, 'XLabel'), 'String', 'Iteration Index', ...
+    'FontSize', fontSizeAxis);
+set(get(hAxes, 'YLabel'), 'String', 'Objective Function Value', ...
+    'FontSize', fontSizeAxis);
+hLegend = ClickableLegend(cMethodNames);
+set(hAxes, 'LooseInset', [0.07, 0.07, 0.07, 0.07]);
+
 if(generateFigures == ON)
-    saveas(hFigure,['Figure', num2str(figureIdx, figureCounterSpec), '.png']);
+    % saveas(hFigure,['Figure', num2str(figureIdx, figureCounterSpec), '.png']);
+    print(hFigure, ['Figure', num2str(figureIdx, figureCounterSpec), '.png'], '-dpng', '-r0'); %<! Saves as Screen Resolution
 end
 
 
