e754c2287beb9d995c2fc51b2dd5caf32599b796
Royi <RoyiAvital@yahoo.com>
2022-02-12 16:45:17 +0200

    Solution to StackExchange Signal Processing Q81493 (https://dsp.stackexchange.com/questions/81493) - Applying 2D Sinc Interpolation for Upsampling in the Fourier Domain (DFT / FFT).

---
+++ SignalProcessing/Q81493/DFTUpSample2D.m
@@ -0,0 +1,76 @@
+function [ mY ] = DFTUpSample2D( mX, vSizeO )
+% ----------------------------------------------------------------------------------------------- %
+% [ mY ] = DFTUpSample2D( mX, vSizeO )
+% Applies Sinc interpolation to the input 2D signal while preserving the
+% Parseval Theorem energy.
+% Input:
+%   - mX                -   2D Signal Samples.
+%                           The samples of the matrix (2D Signal) to be
+%                           interpolated.
+%                           Structure: Matrix (numRowsI x numColsI).
+%                           Type: 'Single' / 'Double'.
+%                           Range: (-inf, inf).
+%   - vSizeO            -   Size Vector of the Output Matrix.
+%                           The number of samples of the output.
+%                           Structure: Vector (1 x 2).
+%                           Type: 'Single' / 'Double'.
+%                           Range: {1, 2, 3, ...}.
+% Output:
+%   - mY                -   2D Output Signal Samples.
+%                           The interpolated matrix samples.
+%                           Equivalent to be interpolated by Sinc (Actually
+%                           by Dirichlet Kernel).
+%                           Structure: Matrix (numRowsO x numColsO).
+%                           Type: 'Single' / 'Double'.
+%                           Range: (-inf, inf).
+% References:
+%   1.  Sinc Interpolation of Discrete Periodic Signals (https://ieeexplore.ieee.org/document/388863).
+%   2.  Comments on "Sinc Interpolation of Discrete Periodic Signals" (https://ieeexplore.ieee.org/document/700979).
+% Remarks:
+%   1.  The function takes care of the even and odd case of the samples. It
+%       assumes the original signal was real hence keeps an hermitian
+%       symmetry and the Parseval Theorem.
+% TODO:
+%   1.  Add support for downsampling.
+%   Release Notes:
+%   -   1.0.000     12/02/2022  Royi Avital
+%       *   First release version.
+% ----------------------------------------------------------------------------------------------- %
+
+arguments
+    mX (:, :) {mustBeNumeric, mustBeReal}
+    vSizeO (1, 2) {mustBeNumeric, mustBeReal, mustBePositive, mustBeInteger}
+end
+
+vSizeI      = size(mX);
+dataClass   = class(mX);
+
+if(any(vSizeO < vSizeI))
+    error('The output size must be equal or larger than the input size');
+end
+
+if(vSizeO == vSizeI)
+    return;
+end
+
+mT = zeros(vSizeO(1), vSizeI(2), dataClass);
+mY = zeros(vSizeO(1), vSizeO(2), dataClass);
+
+for kk = 1:2 %<! 2 dimensions
+    if(kk == 1)
+        numSlices = vSizeI(2);
+    else
+        numSlices = vSizeO(1);
+    end
+    for ii = 1:numSlices
+        if(kk == 1)
+            mT(:, ii) = ifft(SincInterpolationDft(fft(mX(:, ii)), vSizeO(kk)), 'symmetric');
+        else
+            mY(ii, :) = ifft(SincInterpolationDft(fft(mT(ii, :)), vSizeO(kk)), 'symmetric');
+        end
+    end
+end
+
+
+end
+
---
+++ SignalProcessing/Q81493/Q81493.m
@@ -0,0 +1,71 @@
+% StackExchange Signal Processing Q81493
+% https://dsp.stackexchange.com/questions/81493
+% Applying 2D Sinc Interpolation for Upsampling in the Fourier Domain (DFT / FFT)
+% References:
+%   1.  
+% Remarks:
+%   1.  B
+% TODO:
+% 	1.  C
+% Release Notes
+% - 1.0.000     29/12/2021
+%   *   First release.
+
+
+%% General Parameters
+
+subStreamNumberDefault = 79;
+
+run('InitScript.m');
+
+figureIdx           = 0;
+figureCounterSpec   = '%04d';
+
+generateFigures = ON;
+
+
+%% Simulation Parameters
+
+numRowsI = 5000;
+numColsI = 5200;
+
+numRowsO = 10000;
+numColsO = 10400;
+
+sincRadius = 5;
+
+
+%% Generate / Load Data
+
+mX      = GenTest([numRowsI, numColsI], sincRadius);
+mYRef   = GenTest([numRowsO, numColsO], sincRadius);
+
+mY = DFTUpSample2D(mX, [numRowsO, numColsO]);
+
+
+%% Analysis
+
+disp(['The interpolation error is given by: ', num2str(max(abs(mYRef - mY), [], 'all'))]);
+
+
+%% Auxilizary Function
+
+function [ mX ] = GenTest( vSize, sincRadius )
+
+vX = linspace(-sincRadius, sincRadius, vSize(2) + 1);
+vX(end) = [];
+vY = linspace(-sincRadius, sincRadius, vSize(1) + 1);
+vY = vY(:);
+vY(end) = [];
+
+% mX = abs(vX) + abs(vY) + sinc(sqrt(vX .^2 + vY .^2));
+mX = sinc(sqrt(vX .^2 + vY .^2));
+
+end
+
+
+%% Restore Defaults
+
+% set(0, 'DefaultFigureWindowStyle', 'normal');
+% set(0, 'DefaultAxesLooseInset', defaultLoosInset);
+
