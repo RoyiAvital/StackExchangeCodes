4be33c18ef80d5d8bf9a2fc4db79bd33660bcbf7
Royi <RoyiAvital@yahoo.com>
2022-05-06 07:58:35 +0300

    Solution to StackExchange Signal Processing Q82711 (https://dsp.stackexchange.com/questions/82711) - A Machine Learning Alternative to Matched Filter.

--- SignalProcessing/Q82711/Q82711.py
+++ SignalProcessing/Q82711/Q82711.py
@@ -46,7 +46,7 @@ print('Found GPU at: {}'.format(device_name))
 
 
 
-# %% Load Data
+# %% Load Train Data
 
 dMatFile = sio.loadmat('TrainSet.mat')
 mData = dMatFile['mData']
@@ -54,19 +54,48 @@ vLabels = np.squeeze(dMatFile['vLabels'], axis = 1) #<! Labels {0, 1, ..., 99}
 vSignal = np.squeeze(dMatFile['vSignal'], axis = 1)
 
 mX = np.transpose(mData, axes = [1, 0]) #<! Batches before channels
-vY = vLabels - 1 #<! Labels {0, 1, ..., 99}
+mX = np.float32(mX[..., np.newaxis])
+vY = np.float32(vLabels - 1) #<! Labels {0, 1, ..., 99}
+
+
+#%% Load Validation Data
+
+dMatFile = sio.loadmat('ValidationSet.mat')
+mData = dMatFile['mData']
+vLabels = np.squeeze(dMatFile['vLabels'], axis = 1) #<! Labels {0, 1, ..., 99}
+vSignal = np.squeeze(dMatFile['vSignal'], axis = 1)
+
+mXVal = np.transpose(mData, axes = [1, 0]) #<! Batches before channels
+mXVal = np.float32(mXVal[..., np.newaxis])
+vYVal = np.float32(vLabels - 1) #<! Labels {0, 1, ..., 99}
 
 
 # %% Keras Model
 
 modelNet = keras.Sequential()
-modelNet.add(keras.Input(shape = (mData.shape[0], 1)))
+# modelNet.add(keras.Input(shape = (mData.shape[0], 1)))
 modelNet.add(keras.layers.Conv1D(32, vSignal.shape[0], activation = 'relu'))
-modelNet.add(keras.layers.Conv1D(64, 51, activation = 'relu'))
+modelNet.add(keras.layers.Conv1D(48, 51, activation = 'relu'))
 modelNet.add(keras.layers.Conv1D(64, 25, activation = 'relu'))
+modelNet.add(keras.layers.Conv1D(64, 13, activation = 'relu'))
+modelNet.add(keras.layers.Flatten())
 modelNet.add(keras.layers.Dense(units = 100, activation = 'softmax'))
 
-modelNet.summary()
+# modelNet.summary()
+
+
+#%% Defining CheckPoints
+
+folderName = 'CheckPoints'
+checkPointFileName = r'{epoch:03d}-{val_accuracy:.4f}.hdf5'
+os.mkdir(folderName) #<! Make sure the folder exist in case of a file
+checkPointsFilePath = os.path.join(folderName, checkPointFileName)
+modelCheckPointCallback = tf.keras.callbacks.ModelCheckpoint(
+    filepath = checkPointsFilePath, #<! File to save
+    save_weights_only = True, #<! Don't save the whole model but weights only
+    monitor = 'val_accuracy', #<! Measure of performance
+    mode = 'max', #<! How to compare performance as in `monitor`
+    save_best_only = True) #<! Save it only if it is better than current
 
 
 # %% Model Compilation
@@ -75,7 +104,7 @@ initLr      = 0.00045 #<! Relatively high
 lrSchedule  = keras.optimizers.schedules.ExponentialDecay(initLr, decay_steps = 1407, decay_rate = 0.96, staircase = True) #<! Today we have much more effective policies
 hOpt        = keras.optimizers.Adam(learning_rate = lrSchedule, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-07, amsgrad = False, name = "Adam")
 
-modelNet.compile(optimizer = hOpt, loss = keras.losses.categorical_crossentropy, metrics = ['accuracy']) #<! A scheduler automatically create a TensorBoard callback
+modelNet.compile(optimizer = hOpt, loss = keras.losses.SparseCategoricalCrossentropy(), metrics = ['accuracy']) #<! A scheduler automatically create a TensorBoard callback
 
 logDir = os.path.join("logs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 # In order to run it from command line: tensorboard --logdir logs
@@ -83,10 +112,17 @@ callBackTB = tf.keras.callbacks.TensorBoard(logDir, histogram_freq = 1) #<! Tens
 
 # %% Training
 
-batchSize = 128
+batchSize = 48
 numEpochs = 21
 
 # modelNet.fit(X_train, y_train, batch_size = batchSize, epochs = numEpochs, verbose = 1, validation_data = (X_test, y_test), callbacks = [callBackTB])
-modelNet.fit(mX, vY, batch_size = batchSize, epochs = numEpochs, verbose = 1, callbacks = [callBackTB])
+modelNet.fit(mX, vY, batch_size = batchSize, epochs = numEpochs, verbose = 1, validation_data = (mXVal, vYVal), callbacks = [callBackTB, modelCheckPointCallback])
 
-# %%
+# %% Predicting
+
+# modelNet.load_weights(os.path.join('CheckPoints', '009-0.3013.hdf5'))
+
+# mY = modelNet.predict(mXVal)
+# vYPred = np.argmax(mY, axis = 1)
+# dMatFileSave = {'vYPred': vYPred}
+# sio.savemat('PythonData.mat', dMatFileSave) #<! Remember to add 1 in MATLAB
