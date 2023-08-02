4c78aa1457230ca8a95f6a40792d0f4ff500c4d5
Royi <RoyiAvital@yahoo.com>
2023-08-02 13:10:11 +0300

    StackExchange Signal Processing
    Solution to StackExchange Signal Processing Q48233 (https://dsp.stackexchange.com/questions/48233) - Curve Fit of Step Function with Boundary on the 2nd Derivative.

---
+++ SignalProcessing/Q48233/Q48233.jl
@@ -0,0 +1,129 @@
+# StackExchange Signal Processing Q48233
+# https://dsp.stackexchange.com/questions/48233
+# Curve Fit of Step Function with Boundary on the 2nd Derivative
+# References:
+#   1.  
+# Remarks:
+#   1.  Use in Julia as following:
+#       -   Move to folder using `cd(raw"<PathToFolder>");`.
+#       -   Activate the environment using `] activate .`.
+#       -   Instantiate the environment using `] instantiate`.
+#   2.  fd
+# TODO:
+# 	1.  C
+# Release Notes Royi Avital RoyiAvital@yahoo.com
+# - 1.0.000     02/08/2023  Royi Avital
+#   *   First release.
+
+## Packages
+
+# Internal
+using Printf;
+# External
+using Optim;
+using PlotlyJS;
+
+## Constants & Configuration
+
+# Display UIntx numbers as integers
+Base.show(io::IO, x::T) where {T<:Union{UInt, UInt128, UInt64, UInt32, UInt16, UInt8}} = Base.print(io, x)
+
+## General Parameters
+
+figureIdx = 0;
+
+exportFigures = false;
+
+## Functions
+
+function Sigmoid( x :: T; α :: T = 1 ) where {T <: AbstractFloat}
+
+    return inv(exp(-α * x) + one(T));
+
+end
+
+function ∇Sigmoid( x :: T; α :: T = 1 ) where {T <: AbstractFloat}
+
+    σ = Sigmoid(x; α = α);
+    return α * σ * (1 - σ);
+
+end
+
+function ∇²Sigmoid( x :: T; α :: T = 1 ) where {T <: AbstractFloat}
+
+    σ = Sigmoid(x; α = α);
+    return α * α * σ * (1 - σ) * (2 - σ);
+
+end
+
+
+## Parameters
+
+numSamples    = 2000;
+supportRadius = 10;
+
+vα = [1.0, 2.0, 3.0];
+vε = [0.05, 0.075];
+
+## Load / Generate Data
+
+numσ = length(vα);
+numε = length(vε);
+
+vX = LinRange(-supportRadius, supportRadius, numSamples);
+vY = ones(numSamples);
+vY[vX .< 0] .= 0;
+mσ = zeros(numSamples, numσ);
+
+for ii in 1:numσ
+    mσ[:, ii] = Sigmoid.(vX; α = vα[ii]);
+end
+
+## Analysis
+
+# Find Optimal α per Boundary
+
+vαOpt = zeros(numε);
+
+for ii in 1:numε
+    sOptRes = optimize(α -> -maximum(abs.(∇²Sigmoid.(vX, α = α))), 0.001, 100, Brent());
+    vαOpt[ii] = q;
+end
+
+## Display Results
+
+# Display the function variants
+vTr = [scatter(;x = vX, y = vY, mode = "lines", name = "Heavy Step")];
+for ii in 1:numσ
+    nameStr = @sprintf "Sigmoid α = %0.2f" vα[ii];
+    push!(vTr, scatter(;x = vX, y = mσ[:, ii], mode = "lines", name = nameStr))
+end
+oLyt = Layout(;title = "Function Variants");
+hP = plot(vTr, oLyt);
+
+display(hP);
+
+figureIdx += 1;
+
+if exportFigures
+    fileName = @sprintf "Figure%04i.png" figureIdx;
+    savefig(hP, fileName);
+end
+
+# Display the results of optimization
+vTr = [scatter(;x = vX, y = vY, mode = "lines", name = "Heavy Step")];
+for ii in 1:numσ
+    nameStr = @sprintf "Sigmoid α = %0.2f" vα[ii];
+    push!(vTr, scatter(;x = vX, y = mσ[:, ii], mode = "lines", name = nameStr))
+end
+oLyt = Layout(;title = "Function Variants");
+hP = plot(vTr, oLyt);
+
+display(hP);
+
+figureIdx += 1;
+
+if exportFigures
+    fileName = @sprintf "Figure%04i.png" figureIdx;
+    savefig(hP, fileName);
+end
