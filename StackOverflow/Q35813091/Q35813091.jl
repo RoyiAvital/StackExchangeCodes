17400aab4102777868939ef2062a461cfb103187
Royi <RoyiAvital@yahoo.com>
2023-11-26 00:10:02 +0200

    StackOverflow
    Solution to Q35813091 (https://stackoverflow.com/questions/35813091) - Optimization of a Trace Operator with Symmetric, PSD and Unit Trace Constraints.

--- StackOverflow/Q35813091/Q35813091.jl
+++ StackOverflow/Q35813091/Q35813091.jl
@@ -1,6 +1,6 @@
 # StackOverflow Q35813091
 # https://stackoverflow.com/questions/35813091
-# Optimize the Trace with PSD and Trace Constraints.
+# Optimization of a Trace Operator with Symmetric, PSD and Unit Trace Constraints.
 # References:
 #   1.  
 # Remarks:
@@ -23,7 +23,7 @@ using Printf;
 using Random;
 # External
 using Convex;
-using MAT;
+using FastLapackInterface;
 using PlotlyJS;
 using SCS;
 using StableRNGs;
@@ -44,49 +44,80 @@ exportFigures = false;
 
 ## Functions
 
+function hUpMat!( mU :: Matrix{T}, vY :: Vector{T}, arrIdx :: N ) where {T <: AbstractFloat, N <: Integer}
+    mU[:, arrIdx] .= vY;
+    return mU;
+end
+
 function ProjSymMatSet( mX :: Matrix{T} ) where {T <: AbstractFloat}
-    return (mX' .+ mX) ./ T{2.0};
+    # `mX` assumed to be square
+    return (mX' .+ mX) ./ T(2.0);
 end
 
 function ProjPsdMatSet( mX :: Matrix{T} ) where {T <: AbstractFloat}
+    # `mX` assumed to be symmetric
     sEigFac = eigen(mX);
-    return sEigFac.vectors * diagm(max.(sEigFac.values, 0)) * sEigFac.vectors';
+    return sEigFac.vectors * diagm(max.(sEigFac.values, zero(T))) * sEigFac.vectors';
 end
 
 function ProjUniTrMatSet( mX :: Matrix{T} ) where {T <: AbstractFloat}
-    return mX .- (((tr(mX) - one(T)) / size(mX, 1)) * I);
+    # `mX` assumed to be square
+    return mX .- (((tr(mX) - one(T)) / size(mX, 1)) * I(size(mX, 1)));
 end
 
-function ProjSetConDykstra( mU :: Matrix{T}, mZ :: Matrix{T}, vProjFun :: Vector{<: Function}, numIterations :: N ) where {T <: AbstractFloat, N <: Integer}
+function ProjUniTrMatSet!( mX :: Matrix{T} ) where {T <: AbstractFloat}
+    # `mX` assumed to be square
+    mX .-= (((tr(mX) - one(T)) / size(mX, 1)) * I(size(mX, 1)));
+end
+
+function ProjSPSDMatSet!( mX :: Matrix{T}; numIter :: N = 1000, δ :: T = 1e-6 ) where {T <: AbstractFloat, N <: Integer}
+    # `mX` assumed to be a square matrix
+    sEigWs = EigenWs(mX, rvecs = true);
+
+    mY = Matrix{T}(undef, size(mX)); #<! Previous iteration
+    mT = Matrix{T}(undef, size(mX)); #<! Buffer
+
+    for ii ∈ 1:numIter
+        copy!(mY, mX);
+        mX .= (mX' .+ mX) ./ T(2.0);
+        sLapEig = LAPACK.geevx!(sEigWs, 'N', 'N', 'V', 'N', mX);
+        sEigFac = LinearAlgebra.Eigen(sLapEig[2], sLapEig[5]);
+        sEigFac.values .= max.(sEigFac.values, zero(T));
+        mT .= sEigFac.values .* sEigFac.vectors';
+        mul!(mX, sEigFac.vectors, mT);
+        mY .= abs.(mY .- mX);
+        if (maximum(mY) < δ)
+            break;
+        end
+    end
+
+    return mX;
+    
+end
+
+function ProjSetConDykstra( mU :: Matrix{T}, mZ :: Matrix{T}, vProjFun :: Vector{<: Function}; numIterations :: N = 1000, δ :: T = 1e-6 ) where {T <: AbstractFloat, N <: Integer}
+    # Projects mU[:, end] on the intersection of vProjFun
+    # The initialization is assumed to be on mU[:, end].
 
     numSets = size(mU, 2);
 
     for kk ∈ 1:numIterations
-        mU[:, 1] .= vProjFun[ii](mU[:, end] .+ mZ[:, 1]);
+        mU[:, 1] .= vProjFun[1](mU[:, end] .+ mZ[:, 1]);
         mZ[:, 1] .= mU[:, end] .+ mZ[:, 1] .- mU[:, 1];
         for ii ∈ 2:numSets
-            mU[:, ii] .= vProjFun[ii](mZ[:, ii - 1]);
+            mU[:, ii] .= vProjFun[ii](mU[:, ii - 1] .+ mZ[:, ii]);
             mZ[:, ii] .= mU[:, ii - 1] .+ mZ[:, ii] .- mU[:, ii];
+        end
+        if (maximum(abs.(mU[:, end] - mU[:, 1])) < δ)
+            break;
         end 
     end
 
-end
-
-function ADMM!(mX :: Matrix{T}, vZ :: Vector{T}, vU :: Vector{T}, hProxF :: Function, hProxG :: Function; ρ :: T = 2.5) where {T <: AbstractFloat, F <: Function}
-
-    numIterations = size(mX, 2);
-    
-    for ii ∈ 2:numIterations
-        vX = @view mX[:, ii];
-
-        vX .= hProxF(vZ - vU, ρ);
-        vZ .= hProxG(mA * vX + vU, 1 / ρ);
-        vU .= vU + mA * vX - vZ;
-    end
+    return mU[:, numSets];
 
 end
 
-function PD3O!(mX :: Matrix{T}, vS :: Vector{T}, vX̄ :: Vector{T}, vT :: Vector{T}, h∇f :: Function, hProxG :: Function, hProxH :: Function, γ :: T, λ :: T) where {T <: AbstractFloat, F <: Function}
+function PD3O!(mX :: Matrix{T}, vS :: Vector{T}, vX̄ :: Vector{T}, vT :: Vector{T}, hA :: Function, hAᵀ :: Function, h∇f :: Function, hProxG :: Function, hProxH :: Function, γ :: T, λ :: T) where {T <: AbstractFloat}
 
     numIterations = size(mX, 2);
     δ = λ / γ;
@@ -97,10 +128,10 @@ function PD3O!(mX :: Matrix{T}, vS :: Vector{T}, vX̄ :: Vector{T}, vT :: Vector
     for ii ∈ 2:numIterations
         vT .= vX - γ * h∇f(vX); #<! Buffer (vXH)
         
-        vS .= vS + δ * mA * vX̄;
+        vS .= vS + δ * hA(vX̄);
         vS .= vS - δ * hProxH(μ * vS, μ);
         vX = @view mX[:, ii];
-        vX .= hProxG(vT - γ * mA' * vS, γ);
+        vX .= hProxG(vT - γ * hAᵀ(vS), γ);
         vX̄ .= 2vX - vT - γ * h∇f(vX);
     end
 
@@ -112,22 +143,16 @@ end
 # Data
 numRows = 5;
 numCols = numRows; #<! Symmetric Matrix
-valA    = 0.23;
-valB    = 1.05;
 δ       = 1e6;
 δTol    = 1e-5;
 
 # Solvers
-numIterations = 25000;
+numIterations = 250;
 
 # Projected Gradient Descent
-
-# ADMM Solver
-ρ = 2.5;
+η = 0.005;
 
 # PD3O
-
-# Chamoblle Pock
 
 ## Generate / Load Data
 oRng = StableRNG(1234);
@@ -136,17 +161,12 @@ mB = randn(oRng, numRows, numCols);
 mB = mB' * mB + 0.1I;
 mB = mB + mB';
 
-# sEigFac = eigen(mA); #<! Needed for efficient solution of the linear system
-
-# mX = zeros(numCols, numIterations);
-# vX = vX = mA \ (((valA + valB) / 2) * ones(numCols));
-# mX[:, 1] = vX;
-
 # See https://discourse.julialang.org/t/73206
 hδFun( mX :: Matrix{<: AbstractFloat} ) = δ * !(isapprox(tr(mX), 1; atol = δTol) && isapprox(mX, mX'; atol = δTol) && (eigmin(mX) > -δTol));
-hObjFun( mX :: Matrix{<: AbstractFloat} ) = tr(mX * mB) + hδFun(mX);
+hObjFun( mX :: Matrix{<: AbstractFloat} ) = tr(mX * mB);# + hδFun(mX); #<! tr(mA * mB) = sum(mA' .* mB);
 
 dSolvers = Dict();
+
 
 ## Analysis
 
@@ -160,116 +180,80 @@ mXRef = mX0.value
 optVal = sConvProb.optval;
 
 # Projected Gradient Descent
-vProjFun = [ProjSymMatSet, ProjPsdMatSet, ProjUniTrMatSet];
-mU = zeros(numRows * numCols, length(vProjFun));
-mZ = zeros(numRows * numCols, length(vProjFun));
-hUpMat(mU :: Matrix{T}, vY :: Vector{T}) {T <: AbstractFloat} = mU;
-∇F( vY :: Vector{T} ) where {T <: AbstractFloat} = vec(mB);
-hProjFun( vY :: Vector{T} ) where {T <: AbstractFloat} = ProjSetConDykstra(hUpMat!(mU, vY), mZ, 2000);
+# Using Dykstra for projection onto intersection of Convex sets.
+methodName = "PGD";
+
+hVecToMat( vY :: Vector{T} ) where {T <: AbstractFloat} = reshape(vY, numRows, numCols);
+vProjFun = [vec ∘ ProjSymMatSet ∘ hVecToMat, vec ∘ ProjPsdMatSet ∘ hVecToMat, vec ∘ ProjUniTrMatSet ∘ hVecToMat];
+numSets = length(vProjFun);
+mU = zeros(numRows * numCols, numSets);
+mZ = zeros(numRows * numCols, numSets);
+
+∇F( vY :: Vector{T} ) where {T <: AbstractFloat} = vec(mB');
+hProjFun( vY :: Vector{T} ) where {T <: AbstractFloat} = ProjSetConDykstra(hUpMat!(mU, vY, numSets), mZ, vProjFun);
 
 mX = zeros(numRows * numCols, numIterations);
 
 for ii ∈ 2:numIterations
-    mX[:, ii] = mX[:, ii - 1] .- η * ∇F(mX[:, ii - 1]);
-    mX[:, ii] = hProjFun(mX[:, ii]);
+    mX[:, ii] = mX[:, ii - 1] .- η * ∇F(mX[:, ii - 1]); #<! Gradient step
+    mX[:, ii] = hProjFun(mX[:, ii]); #<! Projection step
 end
 
-
-# ADMM
-# Solves: f(x) + g(z) subject to Px + Qz + r = 0
-# f(x) = 0.5 * x' * A * x -> Prox_f(y) = (λ * A' * A + A) \ (λ * A' * y)
-# g(x) = δ(A x) ∈ [a, b] -> Prox_g(y) = clamp(y, a, b)
-# P = A, Q = -I, r = 0
-# methodName = "ADMM";
-
-# hD( λ :: T ) where {T <: AbstractFloat} = (λ .* sEigFac.values) ./ ((λ .* (sEigFac.values .^ 2)) .+ sEigFac.values);
-# hProxF( vY :: Vector{T}, λ :: T ) where {T <: AbstractFloat} = sEigFac.vectors * (hD(λ) .* (sEigFac.vectors' * vY));
-# hProxG( vY :: Vector{T}, λ :: T ) where {T <: AbstractFloat} = clamp.(vY, valA, valB);
-
-# vZ = mA * mX[:, 1];
-# vU = zeros(numCols);
-
-# ADMM!(mX, vZ, vU, hProxF, hProxG; ρ = ρ);
-
-# dSolvers[methodName] = [hObjFun(mX[:, ii]) for ii ∈ 1:size(mX, 2)];
+dSolvers[methodName] = [hObjFun(reshape(mX[:, ii], numRows, numCols)) for ii ∈ 1:size(mX, 2)];
 
 
 # PD3O
 # Solves: \arg \min_x f(x) + g(x) + h(A * x)
-# f(x) = 0.5 * x' * A * x
-# g(x) = 0 -> Prox_g(y) = y
-# h(x) = δ(A x) ∈ [a, b] -> Prox_h(y) = clamp(y, a, b)
+# f(x) = B * X 
+# g(x) = δ_S_+(x) -> Prox_g(y) = Proj_S_+(y)
+# h(x) = δ_Tr(x) ∈ [a, b] -> Prox_h(y) = clamp(y, a, b)
 # Useful as it doesn't require solving big linear equation.
 # γ - Primal step size, δ - Dual step size.
 
-# methodName = "PD3O";
+methodName = "PD3O";
 
-# valL = opnorm(mA)
-# β = 1 / valL;
-# γ = 1.8β; #<! γ < 2β (Like a primal step size for Gradient Descent)
-# λ = 0.9β * β; #<! γ * δ < β²
-# μ = γ / λ; #<! In the paper 1/δ
-# δ = λ / γ;
+fill!(mX, zero(eltype(mX)));
 
-# h∇f( vX :: AbstractVector{T} ) where {T <: AbstractFloat} = mA * vX;
-# hProxG( vY :: Vector{T}, λ :: T ) where {T <: AbstractFloat} = vY;
-# hProxH( vY :: Vector{T}, λ :: T ) where {T <: AbstractFloat} = clamp.(vY, valA, valB);
+valL = 1; #<! Since h(A * x) = h(I * x) = h(x)
+β = 1 / opnorm(mB); #<! By f(x)
+γ = 1.8β; #<! γ < 2β (Like a primal step size for Gradient Descent)
+λ = 250β * β; #<! γ * δ < (1 / (L * L))
+μ = γ / λ; #<! In the paper 1/δ
+δ = λ / γ;
 
-# vX̄ = copy(mX[:, 1]);
-# vS = mA * mX[:, 1];
-# vT = zeros(numCols);
+hA( vX :: AbstractVector{T} ) where {T <: AbstractFloat} = vX;
+hAᵀ( vX :: AbstractVector{T} ) where {T <: AbstractFloat} = vX;
+h∇f( vX :: AbstractVector{T} ) where {T <: AbstractFloat} = vec(mB');
+hProxG( vY :: Vector{T}, λ :: T ) where {T <: AbstractFloat} = vec(ProjSPSDMatSet!(reshape(vY, numRows, numCols)));
+hProxH( vY :: Vector{T}, λ :: T ) where {T <: AbstractFloat} = vec(ProjUniTrMatSet!(reshape(vY, numRows, numCols)));
 
-# PD3O!(mX, vS, vX̄, vT, h∇f, hProxG, hProxH, γ, λ);
+vX̄ = copy(mX[:, 1]);
+vS = copy(mX[:, 1]);
+vT = zeros(numRows * numCols);
 
-# dSolvers[methodName] = [hObjFun(mX[:, ii]) for ii ∈ 1:size(mX, 2)];
+PD3O!(mX, vS, vX̄, vT, hA, hAᵀ, h∇f, hProxG, hProxH, γ, λ);
 
-
-# Dual Prox
-# Solves: arg min_x f(A * x) + g(x)
-# f(A * x) = δ(A x) ∈ [a, b] -> Prox_h(y) = clamp(y, a, b)
-# g(x) = x' * A * x
-# The Prox of g(x) will require solving a Linear System equation.
-
-# methodName = "ChambollePock";
-
-# valL = opnorm(mA' * mA);
-
-# τ = sqrt(1 / (1.05 * valL));
-# σ = sqrt(1 / (1.05 * valL));
-# θ = 1.0;
-
-# hProxF( vY :: Vector{T}, λ :: T ) where {T <: AbstractFloat} = clamp.(vY, valA, valB);
-# hProxF⁺( vY :: Vector{T}, λ :: T ) where {T <: AbstractFloat} = vY - λ * hProxF(vY ./ λ, 1 / λ); #<! Prox of conjugate
-# hD( λ :: T ) where {T <: AbstractFloat} = 1 ./ ((λ .* sEigFac.values) .+ 1);
-# hProxG( vY :: Vector{T}, λ :: T ) where {T <: AbstractFloat} = sEigFac.vectors * (hD(λ) .* (sEigFac.vectors' * vY));
-# # hProxG( vY :: Vector{T}, λ :: T ) where {T <: AbstractFloat} = (λ * mA + I) \ vY;
-
-# vP = mX[:, 1];
-# vX̄ = mX[:, 1];
-
-# ChamPock!(mX, vP, vX̄, hProxF⁺, hProxG, σ, τ; θ = θ)
-
-# dSolvers[methodName] = [hObjFun(mX[:, ii]) for ii ∈ 1:size(mX, 2)];
+dSolvers[methodName] = [hObjFun(reshape(mX[:, ii], numRows, numCols)) for ii ∈ 1:size(mX, 2)];
 
 
 ## Display Results
 
-# figureIdx += 1;
+figureIdx += 1;
 
-# vTr = Vector{GenericTrace{Dict{Symbol, Any}}}(undef, length(dSolvers));
+vTr = Vector{GenericTrace{Dict{Symbol, Any}}}(undef, length(dSolvers));
 
-# # shapeLine = vline(sOptRes.minimizer, line_color = "green", name = "Optimal Value");
-# for (ii, methodName) in enumerate(keys(dSolvers))
-#     vTr[ii] = scatter(x = 1:numIterations, y = 20 * log10.(abs.(dSolvers[methodName] .- optVal) ./ abs(optVal)), 
-#                mode = "lines", text = methodName, name = methodName, line = attr(width = 3.0))
-# end
-# oLayout = Layout(title = "Objective Function, Condition Number = $(cond(mA))", width = 600, height = 600, hovermode = "closest",
-#                  xaxis_title = "Iteration", yaxis_title = raw"$$\frac{ \left| {f}^{\star} - {f}_{i} \right| }{ \left| {f}^{\star} \right| }$ [dB]$");
+# shapeLine = vline(sOptRes.minimizer, line_color = "green", name = "Optimal Value");
+for (ii, methodName) in enumerate(keys(dSolvers))
+    vTr[ii] = scatter(x = 1:numIterations, y = 20 * log10.(abs.(dSolvers[methodName] .- optVal) ./ abs(optVal)), 
+               mode = "lines", text = methodName, name = methodName, line = attr(width = 3.0))
+end
+oLayout = Layout(title = "Objective Function, Condition Number = $(@sprintf("%0.3f", cond(mB)))", width = 600, height = 600, hovermode = "closest",
+                 xaxis_title = "Iteration", yaxis_title = raw"$$\frac{ \left| {f}^{\star} - {f}_{i} \right| }{ \left| {f}^{\star} \right| }$ [dB]$");
 
-# hP = plot(vTr, oLayout);
-# display(hP);
+hP = plot(vTr, oLayout);
+display(hP);
 
-# if (exportFigures)
-#     figFileNme = @sprintf("Figure%04d.png", figureIdx);
-#     savefig(hP, figFileNme);
-# end
+if (exportFigures)
+    figFileNme = @sprintf("Figure%04d.png", figureIdx);
+    savefig(hP, figFileNme);
+end
