cae9a9ab881c4964a2054ad1e32a863b326b0666
Royi <RoyiAvital@yahoo.com>
2025-08-18 09:37:40 +0300

    StackExchange Mathematics
    Solution to StackExchange Mathematics Q2154648 (https://math.stackexchange.com/questions/2154648) - Manual Implementation of the Kernel SVM to Match SciKit Learn.

--- Mathematics/Q264099/SolveShiftedLinearSysetm.jl
+++ Mathematics/Q264099/SolveShiftedLinearSysetm.jl
@@ -1,18 +1,6 @@
-# StackExchange Mathematics Q264099
-# https://math.stackexchange.com/questions/264099
-# Solving the Primal Kernel SVM Problem.
-# References:
-#   1.  A
-# Remarks:
-#   1.  Use in Julia as following:
-#       -   Move to folder using `cd(raw"<PathToFolder>");`.
-#       -   Activate the environment using `] activate .`.
-#       -   Instantiate the environment using `] instantiate`.
-#   2.  A
-# TODO:
-# 	1.  AA.
+# Solving Shifted Linear System with SPD Matrix and Non Trivial Matrix.
 # Release Notes Royi Avital RoyiAvital@yahoo.com
-# - 1.0.000     12/08/2025  Royi Avital
+# - 1.0.000     17/08/2025  Royi Avital
 #   *   First release.
 
 ## Packages
@@ -23,15 +11,12 @@ using Printf;
 using Random;
 # External
 using BenchmarkTools;
+using PlotlyJS;
 using StableRNGs;
 
 
 ## Constants & Configuration
 RNG_SEED = 1234;
-
-juliaCodePath = joinpath(".", "..", "..", "JuliaCode");
-include(joinpath(juliaCodePath, "JuliaInit.jl"));
-include(joinpath(juliaCodePath, "JuliaVisualization.jl"));
 
 
 ## Settings
@@ -45,25 +30,55 @@ oRng = StableRNG(1234);
 
 ## Functions
 
-function KernelSVM( vα :: Vector{T}, paramB :: T, mK :: Matrix{T}, vY :: Vector{T}, λ :: T; squareHinge :: Bool = false ) where {T <: AbstractFloat}
+function GenData( numRows :: N ) where {N <: Integer}
 
-    numSamples = length(vY);
+    # Problem Data
+    mA = randn(numRows, numRows);
+    mA = mA' * mA;
+    mA = 0.5 * (mA' + mA);
+    mC = randn(numRows, numRows);
+    mC = (mC' * mC) + 0.05 * I;
+    mC = 0.5 * (mC' + mC); #<! SPD Matrix
     
-    # Regularization
-    objVal = 0.5 * λ * dot(vα, mK, vα);
+    vB = randn(numRows);2
+    
+    # Decomposition
+    mU = cholesky(mC).U;
+    vD = mU' \ vB;
+    mE = mU' \ mA / mU; #<! Symmetric (Can be SPD / SPSD as `mA`)
+    sH = hessenberg(mE);
+    sE = eigen(mE);
 
-    # Objective
-    vH = [max(zero(T), T(1) - vY[ii] * (dot(vα, mK[:, ii]) + paramB)) for ii in 1:numSamples];
-    if squareHinge
-        vH = [vH[ii] * vH[ii] for ii in 1:numSamples];
-    end
+    return mA, mC, vB, mU, vD, mE, sH, sE;
 
-    objVal += sum(vH);
+end
 
-    # Vectorized form
-    # objVal = 0.5 * λ * dot(vα, mK, vα) + sum(max.(zero(T), T(1) .- vY .* (mK * vα .+ paramB)));
+function SolveDirect( mA :: Matrix{T}, mC :: Matrix{T}, α :: T,  vB :: Vector{T}, vX :: Vector{T} ) where {T <: AbstractFloat}
 
-    return objVal;
+    sC = cholesky(mA + α * mC);
+    ldiv!(vX, sC, vB);
+
+    return vX;
+
+end
+
+function SolveHessenberg( sH :: H, α :: T, vD :: Vector{T}, mU :: UT, vX :: Vector{T}, vY :: Vector{T} ) where {T <: AbstractFloat, H <: Hessenberg, UT <: UpperTriangular}
+
+    ldiv!(vY, sH + α * I, vD);
+    ldiv!(vX, mU, vY);
+
+    return vX;
+
+end
+
+function SolveEigen( sE :: E, α :: T, vD :: Vector{T}, mU :: UT, vX :: Vector{T}, vY :: Vector{T} ) where {T <: AbstractFloat, E <: Eigen, UT <: UpperTriangular}
+
+    mul!(vX, sE.vectors', vD);
+    vX ./= (sE.values .+ α);
+    mul!(vY, sE.vectors, vX);
+    ldiv!(vX, mU, vY);
+
+    return vX;
 
 end
 
@@ -72,58 +87,68 @@ end
 
 # Data
 numRows = 10;
+# vR = collect(250:250:1000);
+vR = collect(250:250:3000);
 
-# Solver
-α  = 1.0;
-vα = collect(LinRange(0.1, 1.5, 10));
-
-# Visualization
+# Model
+α  = 0.275;
 
 
 ## Load / Generate Data
 
+mA, mC, vB, mU, vD, mE, sH, sE = GenData(numRows);
+vY = zeros(numRows); #<! Buffer
+vX = zeros(numRows); #<! Buffer
+
 
 ## Analysis
 
-mA = randn(numRows, numRows);
-mA = mA' * mA;
-mA = 0.5 * (mA' + mA);
-mC = (mC' * mC) + 0.05 * I;
-mC = 0.5 * (mC' + mC); #<! SPD Matrix
+# Solve
+vXRef = copy(SolveDirect(mA, mC, α, vB, vX));
 
-vB = randn(numRows);
+# Test for correctness
+# Hessenberg
+vX = SolveHessenberg(sH, α, vD, mU, vX, vY);
+println(norm(vX - vXRef));
 
-# Pre Process
-mU = cholesky(mX).U;
-vY = mU * vX;
-vD = mU' \ vB;
-mE = mU' \ mA / mU; #<! Symmetric (Can be SPD / SPSD as `mA`)
-mH = hessenberg(mE);
+# Eigen
+vX = SolveEigen(sE, α, vD, mU, vX, vY);
+println(norm(vX - vXRef));
+
+mRunTime = zeros(length(vR), 3);
+
+# Test for Run Time
+for (ii, nn) in enumerate(vR)
+    mA, mC, vB, mU, vD, mE, sH, sE = GenData(nn);
+    vY = zeros(nn); #<! Buffer
+    vX = zeros(nn); #<! Buffer
+
+    mRunTime[ii, 1] = @belapsed SolveDirect($mA, $mC, $α, $vB, $vX) seconds = 0.3;
+    mRunTime[ii, 2] = @belapsed SolveHessenberg($sH, $α, $vD, $mU, $vX, $vY) seconds = 0.3;
+    mRunTime[ii, 3] = @belapsed SolveEigen($sE, $α, $vD, $mU, $vX, $vY) seconds = 0.3;
+end
+
+mRunTime *= 1e3; #<! Sec -> Mili Sec
 
 ## Display Analysis
 
 figureIdx += 1;
 
-# startX, endX = CalcXRangeLine(vW, tuRange);
-# vXX = collect(LinRange(startX, endX, numGridPts));
-# vYY = CalcLineVal(vW, vXX);
+sTr1 = scatter(x = vR, y = mRunTime[:, 1], mode = "lines", text = "Direct Solver", name = "Direct Solver",
+                line_width = 2);
+sTr2 = scatter(x = vR, y = mRunTime[:, 2], mode = "lines", text = "Hessenberg Solver", name = "Hessenberg Solver",
+                line_width = 2);
+sTr3 = scatter(x = vR, y = mRunTime[:, 3], mode = "lines", text = "Eigen Solver", name = "Eigen Solver",
+                line_width = 2);
+sLayout = Layout(title = "Solver Run Time per Matrix Size", width = 600, height = 600, hovermode = "closest",
+                xaxis_title = "Number of Rows", yaxis_title = "Run Time [Mili Sec]",
+                legend = attr(yanchor = "top", y = 0.99, xanchor = "left", x = 0.01));
 
-# sTr1 = scatter(x = mX[:, 1], y = mX[:, 2], mode = "markers", text = "Data Samples", name = "Data Samples",
-#                 marker = attr(size = 10, color = vY));
-# sTr2 = scatter(x = vXX, y = vYY, 
-#                 mode = "lines", text = "Classifier", name = "Classifier",
-#                 line = attr(width = 2.5));
-# sTr3 = scatter(x = mX[vSuppVecIdx, 1], y = mX[vSuppVecIdx, 2], mode = "markers", text = "Support Vectors", name = "Support Vectors",
-#                 marker = attr(size = 20, color = "rgba(0, 0, 0, 0)", line = attr(color = "k", width = 2)));
-# sLayout = Layout(title = "The Data Samples and Classifier", width = 600, height = 600, hovermode = "closest",
-#                 xaxis_title = "t", yaxis_title = "y", xaxis_range = tuRange, yaxis_range = tuRange,
-#                 legend = attr(yanchor = "top", y = 0.99, xanchor = "right", x = 0.99));
+hP = plot([sTr1, sTr2, sTr3], sLayout);
+display(hP);
 
-# hP = plot([sTr1, sTr2, sTr3], sLayout);
-# display(hP);
-
-# if (exportFigures)
-#     figFileNme = @sprintf("Figure%04d.png", figureIdx);
-#     savefig(hP, figFileNme);
-# end
+if (exportFigures)
+    figFileNme = @sprintf("Figure%04d.png", figureIdx);
+    savefig(hP, figFileNme);
+end
 
